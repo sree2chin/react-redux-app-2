3a4f6407c7fbf0737457cda3e712b718d88da07e
Srikanth <sree2chin@gmail.com>
2018-07-27 14:10:24 +0530

    include header component

---
+++ src/components/header.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Header = () => {   // crated a component
+    return <h1>I am header </h1>
+}
+        
+export default Header
--- src/index.js
+++ src/index.js
@@ -2,8 +2,16 @@ import React from 'react'
 // here it goes to nodemodules and look for foleder react and include the function React. 
 import ReactDOM from 'react-dom'
 
+//COMPONENTS
+import Header from "./components/header"
+
 const App = () => {   // crated a component
-    return <h1>Hello world </h1>
+    return (
+        <div>
+            <Header/>
+            <h1>Hello world</h1>
+        </div>
+    )
 }
              // what we want to output, where we want to output
 ReactDOM.render(<App/>, document.querySelector('#root'));
