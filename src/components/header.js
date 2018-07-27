882663f57783df1cdbe6a22339da524fdd6f2dce
Srikanth <sree2chin@gmail.com>
2018-07-27 17:22:53 +0530

    make onchange work with setState

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,41 @@
 import React from 'react'
 
-const Header = () => {   // crated a component
-    return <h1>I am header </h1>
+// const Header = () => {   // crated a component
+//     return <h1>I am header </h1>
+// }
+
+class Header extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            keywords: ""
+        }
+    }
+    inputChange = function(event) {
+        // console.log(event.target.value);
+        this.setState({
+            keywords: event.target.value
+        })
+    }
+    render() {
+        //inline styles object.. 
+        const styles = {
+            header: {
+                background: '#eeefad'
+            },
+            logo: {
+                color: '#fff',
+                textAlign: 'center'
+            }
+        }
+        return(
+            <header className = "header-cont" style = {styles.header}>
+                <div style = {styles.logo}>Logo</div>
+                <input onChange = {this.inputChange.bind(this)}/>
+                <div>{this.state.keywords}</div>
+            </header>
+        )
+    }
 }
         
 export default Header
